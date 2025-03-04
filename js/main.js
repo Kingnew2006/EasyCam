cc7ec705ce47e06b030e24abc549296efa835a0a
MaivandRahmani <103935508+Kingnew2006@users.noreply.github.com>
2025-03-05 00:18:31 +0300

    main__readymade page added

--- js/main.js
+++ js/main.js
@@ -82,7 +82,26 @@ const MainForm = document.querySelector('.header__main__verification__input')
 MainForm.elements[0].value = 'User1'
 MainForm.elements[1].value = '123456789'
 console.log(MainForm.elements[0].value)  
+let readycard = document.querySelector('.main__readymade__main__card')
+let page__ready = {
 
+clonecard: readycard.cloneNode(true) , 
+clonecard_2: readycard.cloneNode(true) ,
+textnew() {
+this.clonecard.childNodes[3].textContent = 'Для бизнеса' 
+this.clonecard_2.childNodes[3].textContent = 'Для улицы' 
+},
+cards__main: document.querySelector('.main__readymade__main') ,
+cloneing() {
+    this.textnew()
+     this.cards__main.append(this.clonecard) 
+     this.cards__main.append(this.clonecard_2)  
+   
+    
+}
+}
+page__ready.cloneing() 
+ 
 
 
  
