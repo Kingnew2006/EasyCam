c37b7fbb8facab062b6143f56c1a62a9bde9a4e2
MaivandRahmani <103935508+Kingnew2006@users.noreply.github.com>
2025-03-07 21:29:07 +0300

    adding datebase for news and create a full auto news board

--- js/main.js
+++ js/main.js
@@ -101,8 +101,49 @@ cloneing() {
 }
 }
 page__ready.cloneing() 
- 
 
+let newses = []
+let newsboard = document.querySelector('.main__news__board')
+let news = async () => {
+    let res = await fetch('http://localhost:3000/news') 
+    let data = await res.json()
+    newses = data 
 
+}  
+let newsload = async () => {
+  newses.forEach((key) => {
+     let {title , news__date , description } = key 
+         let newscard = document.createElement('div')  
+         newscard.classList.add('main__news__board__card')
+
+        let newstitle = document.createElement('div') 
+         newstitle.classList.add('main__news__board__card__title' ) 
+        newstitle.textContent = `${title}` 
+        newscard.append(newstitle) 
+
+        let twomain = document.createElement('div')
+        twomain.classList.add('main__news__board__card__twomain')
+        newscard.append(twomain)
+
+        let newsdata = document.createElement('div')
+        newsdata.classList.add('main__news__board__card__twomain__date')
+        newsdata.textContent = `${news__date.split("T")[0]}`
+        twomain.append(newsdata) 
+
+        let descriptions = document.createElement('div') 
+        descriptions.classList.add('main__news__board__card__twomain__description') 
+        descriptions.textContent = `${description}`
+        twomain.append(descriptions)
+        
+
+        newsboard.append(newscard)  
+
+  }) 
+}
+
+news().then(()=> {console.log(newses) , newsload()  })
  
  
+   
+
+     
